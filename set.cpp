bb1513a2714939459584f6bc444bd3e3a37579e4
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2023-05-25 10:27:12 +0530

    Create set.cpp

---
+++ set.cpp
@@ -0,0 +1,14 @@
+class Set {
+private:
+    svector<int> elements;
+
+public:
+     //Function to check if an element belongs to the set
+    bool isMember(int element) {
+        for (int i = 0; i < elements.size(); ++i) {
+            if (elements[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    };
